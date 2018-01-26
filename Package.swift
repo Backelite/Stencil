a7f670e6c72825bf039fffc10a80c2ac7e6a6ec0
Philippe Bernery <philippe.bernery@backelite.com>
2018-01-26 14:22:27 +0100

    Update Package.swift for Swift 4

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
   name: "Stencil",
   dependencies: [
-    .Package(url: "https://github.com/kylef/PathKit.git", majorVersion: 0, minor: 9),
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 8),
+    .package(url: "https://github.com/kylef/PathKit.git", from: "0.9.0"),
+    .package(url: "https://github.com/kylef/PathKit.git", from: "0.8.0")
   ]
 )
